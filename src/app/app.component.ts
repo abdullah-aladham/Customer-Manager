4c48f6784c9920fbdf0a848f1ae0b35a82d0afb2
Abdullah Aladham <abdullah.aladham@gmail.com>
2021-11-08 09:31:38 +0200

    commit 8/11

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,10 @@
-import { Component } from '@angular/core';
-
+import { Component, } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { Router } from '@angular/router';
+import { CustomerAddComponent } from './customer-add/customer-add.component';
+//import { CustomerEditComponent } from './customer-edit/customer-edit.component';
+import Customers from './database/Customers.json';
+import { EditCustomerComponent } from './edit-customer/edit-customer.component';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +12,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Customer-Manager1';
+  public customerList:{name:string, City:string,Country:string}[] = Customers ; 
+
+constructor(private router:Router,private dialogRef:MatDialog){}
+ editCustomer(){
+this.dialogRef.open(EditCustomerComponent);
+} 
+addCustomer(){
+  this.dialogRef.open(CustomerAddComponent);
+  } 
 }
