2edfdf52c452d1e3b2c6d2f491dbe36aa52b902c
Abdullah Aladham <abdullah.aladham@gmail.com>
2021-11-08 09:31:53 +0200

    commit 8/11

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import{BrowserAnimationsModule} from'@angular/platform-browser/animations';
+import {MatDialogModule}from '@angular/material/dialog';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+//import { CustomerEditComponent } from './customer-edit/customer-edit.component';
+import { EditCustomerComponent } from './edit-customer/edit-customer.component';
+//import { AddCustomerDirective } from './add-customer.directive';
+import { CustomerAddComponent } from './customer-add/customer-add.component';
+import {SimpleModalModule } from 'ngx-simple-modal';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+  //  CustomerEditComponent,
+    EditCustomerComponent,
+    //AddCustomerDirective,
+    CustomerAddComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatDialogModule,
+    SimpleModalModule,
+    SimpleModalModule.forRoot({container:document.body})
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/edit-customer/edit-customer.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+//import Customers from './database/Customers.json';
+//import customerList from '../app.component';
+@Component({
+  selector: 'app-edit-customer',
+  templateUrl: './edit-customer.component.html',
+  styleUrls: ['./edit-customer.component.sass']
+})
+export class EditCustomerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+//clist:Array<customerList>=[];
+}
